abbf976897babfbf653243ff302a611d90f9c9d3
Eugene Lukyantsau <pro100.freeman@gmail.com>
2021-11-07 13:33:15 +0300

    Initial commit

---
+++ app/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
