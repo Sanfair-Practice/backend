8d745b4cca1c4fdf0e8ae62b796a812b89bbbe82
Eugene Lukyantsau <pro100.freeman@gmail.com>
2021-12-04 15:32:25 +0300

    Add ide helper support

--- app/app/Models/Test.php
+++ app/app/Models/Test.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @mixin IdeHelperTest
+ */
 class Test extends Model
 {
     use HasFactory;
