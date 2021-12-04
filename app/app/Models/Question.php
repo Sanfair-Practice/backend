8f1b1c35475851e0ca16830621f6b70bd421884a
Eugene Lukyantsau <pro100.freeman@gmail.com>
2021-12-04 15:31:38 +0300

    Fix missed dependency error

--- app/app/Models/Question.php
+++ app/app/Models/Question.php
@@ -6,14 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Question extends Model
 {
     use HasFactory;
-    use InteractsWithMedia;
 
     protected $casts = [
         'dummy' => 'array',
